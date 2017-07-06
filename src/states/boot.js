f331fc82f739b4179ed3a1af17b653bc88422058
Travis Chase <gaudeon@cpan.org>
2017-07-05 21:25:41 -0600

    added pretend score at top of screen

--- src/states/boot.js
+++ src/states/boot.js
@@ -1,13 +1,26 @@
 // json imports
 import spritesJSON from '../../assets/json/sprites.json';
 
+// web fonts
+import WebFont from 'webfontloader';
+
 // require in other assets to be included but not added to cache at this time
 require('../../assets/images/sprites.png');
+require('../../assets/css/fonts.css'); // arcade-classic
+require('../../assets/fonts/ARCADECLASSIC.TTF'); // arcade-classic
 
 export default class LoadingState extends Phaser.State {
+    init () {
+        // font loading requirements
+       this.requireWebFont('ARCADECLASSIC');
+    }
+
     preload () {
         // load json configuration files
         this.game.cache.addJSON('spritesJSON', null, spritesJSON);
+
+        // font loading (only call loadWebFonts once)
+       this.loadWebFonts('ARCADECLASSIC');
     }
 
     create () {
@@ -18,9 +31,57 @@ export default class LoadingState extends Phaser.State {
             // event.previousSlide, event.currentSlide, event.indexh, event.indexv
             this.state.start('Play', true, false, event.indexh, event.indexv);
         });
+    }
 
-        Reveal.initialize();
+    update () {
+        if (this.areWebfontsLoaded()) {
+            Reveal.initialize();
 
-        this.state.start('Play', true, false);
+            this.state.start('Play', true, false);
+        }
+    }
+
+    requireWebFont (fontKey) {
+        this.webfonts = this.webfonts || {};
+
+        this.webfonts[fontKey] = false;
+    }
+
+    setWebFontLoaded (fontKey) {
+        this.webfonts = this.webfonts || {};
+
+        this.webfonts[fontKey] = true;
+    }
+
+    loadWebFonts (...fontKeys) {
+        let familyList = [];
+
+        _.each(fontKeys, (key) => {
+            familyList.push(key);
+        });
+
+        WebFont.load({
+            active: () => {
+                _.each(fontKeys, (key) => {
+                    this.setWebFontLoaded(key);
+                });
+            },
+            custom: {
+                families: familyList,
+                urls: ['/assets/fonts.css']
+            }
+        });
+    }
+
+    areWebfontsLoaded () {
+        this.webfonts = this.webfonts || {};
+
+        for (let fontKey in this.webfonts) {
+            if (!this.webfonts[fontKey]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- src/states/play.js
+++ src/states/play.js
@@ -13,6 +13,55 @@ export default class PlayState extends Phaser.State {
     }
 
     create () {
+        let screenThird = this.game.width / 3;
+
+        // 1UP
+        const textOneUpX = 0;
+        const textOneUpY = 0;
+        let textOneUp = this.game.add.text(textOneUpX, textOneUpY, '1UP', {
+            fill: 'red',
+            stroke: 'red',
+            font: '32pt ARCADECLASSIC',
+            boundsAlignH: 'center'
+        });
+        textOneUp.setTextBounds(textOneUpX, textOneUpY, screenThird, textOneUpY.height);
+
+        // 1UP Score
+        const oneUpScore = this.slideIndexH * 100;
+        const oneUpScoreFormatted = oneUpScore === 0 ? '0000000' : oneUpScore > 1000 ? '000' + oneUpScore : '0000' + oneUpScore;
+        const textOneUpScoreX = 0;
+        const textOneUpScoreY = 16;
+        let textOneUpScore = this.game.add.text(textOneUpScoreX, textOneUpScoreY, oneUpScoreFormatted, {
+            fill: 'white',
+            stroke: 'white',
+            font: '32pt ARCADECLASSIC',
+            boundsAlignH: 'center'
+        });
+        textOneUpScore.setTextBounds(textOneUpScoreX, textOneUpScoreY, screenThird, textOneUpScoreY.height);
+
+        // High Score
+        const textHighScoreX = screenThird;
+        const textHighScoreY = 0;
+        let textHighScore = this.game.add.text(textHighScoreX, textHighScoreY, 'High   Score', {
+            fill: 'red',
+            stroke: 'red',
+            font: '32pt ARCADECLASSIC',
+            boundsAlignH: 'center'
+        });
+        textHighScore.setTextBounds(textHighScoreX, textHighScoreY, screenThird, textHighScoreY.height);
+
+        // High Score Score
+        const highScoreAmountFormatted = '3333360';
+        const textHighScoreAmountX = screenThird;
+        const textHighScoreAmountY = 16;
+        let textHighScoreAmount = this.game.add.text(textHighScoreAmountX, textHighScoreAmountY, highScoreAmountFormatted, {
+            fill: 'white',
+            stroke: 'white',
+            font: '32pt ARCADECLASSIC',
+            boundsAlignH: 'center'
+        });
+        textHighScoreAmount.setTextBounds(textHighScoreAmountX, textHighScoreAmountY, screenThird, textHighScoreAmountY.height);
+
         let anim;
 
         switch (this.slideIndexH) {
